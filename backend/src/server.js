67259d193d4df57b0a16b8560ea8ad58cdb7ddc1
Luisa Novaes <luisanovaes.arq@gmail.com>
2024-04-16 23:45:26 -0300

    add: update database values using url params on req and resp

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,23 +1,39 @@
 //express server. Run with Node using 'node src/server.js'
-import express from 'express'
+import express from "express";
+
+let articlesInfo = [
+  {
+    name: "learn-react",
+    upvotes: 0,
+  },
+  {
+    name: "learn-node",
+    upvotes: 0,
+  },
+  {
+    name: "mongodb",
+    upvotes: 0,
+  },
+];
 
 const app = express();
-app.use(express.json())
+app.use(express.json());
 
-app.get('/hello',(req, res) => {
-    res.send('Hello');
-});
+app.put("/api/articles/:name/upvote", (req, res) => {
+    const name = req.params.name;   //name inputed in the url (request)
+    const article = articlesInfo.find(article => article.name === name);  //find the article which param value === database value
+    
+    if (article) {
+        article.upvotes += 1;
+        res.send(`The ${name} article now has ${article.upvotes} upvotes!`)
+    } else {
+        res.send('That article doesn\'t exist')
+    }
 
-app.get('/hello/:name',(req, res) => {
-    const name = req.params.name
-    res.send(`Hello, ${name}`);
-});
 
-app.post('/hello',(req, res) => {
-    console.log(req.body)
-    res.send(`Hello, ${req.body.name}!`);
+        
 });
 
 app.listen(8000, () => {
-    console.log('Server is listening on port 8000')
+  console.log("Server is listening on port 8000");
 });
