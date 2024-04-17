609a2f0bb1bc766bce484e9cb13b3e6c09af71b2
Luisa Novaes <luisanovaes.arq@gmail.com>
2024-04-16 21:51:58 -0300

    add: render content according to param

--- src/pages/ArticlePage.jsx
+++ src/pages/ArticlePage.jsx
@@ -1,5 +1,18 @@
+import { useParams } from "react-router-dom";
+import articles from "../article-content";
+
 export const ArticlePage = () => {
+  const params = useParams();
+  const articleId = params.articleId; //the param key is defined in the route /id:paramName, and the value is what is typed in the url
+
+  const article = articles.find((article) => article.name === articleId);
+
   return (
-    <div>ArticlePage</div>
-  )
-}
+    <>
+      <h1>{article.title}</h1>
+      {article.content.map((content) => (
+        <p key={content.id}>{content}</p>
+      ))}
+    </>
+  );
+};
