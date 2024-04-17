b35187f0cfbd9c147742054efd26be4ce13bd380
Luisa Novaes <luisanovaes.arq@gmail.com>
2024-04-16 21:20:06 -0300

    add: Link pages and create navbar

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,23 @@ import { HomePage } from "./pages/HomePage";
 import { ArticleListPage } from "./pages/ArticleListPage";
 import { AboutPage } from "./pages/AboutPage";
 import { ArticlePage } from "./pages/ArticlePage";
+import { Navbar } from "./Navbar";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/about" element={<AboutPage />} />
-          <Route path="/articles" element={<ArticleListPage />} />
-          <Route path="/articles/:articleId" element={<ArticlePage />} />
-        </Routes>
+        <div className="App">
+          <Navbar />
+          <div id='page-body'>
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/about" element={<AboutPage />} />
+              <Route path="/articles" element={<ArticleListPage />} />
+              <Route path="/articles/:articleId" element={<ArticlePage />} />
+            </Routes>
+          </div>
+        </div>
       </BrowserRouter>
     </div>
   );
---
+++ src/Navbar.jsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+
+export const Navbar = () => {
+  return (
+    <nav>
+      <ul>
+        <li><Link to='/'>Home</Link></li>
+        <li><Link to='/about'>About</Link></li>
+        <li><Link to='/articles'>Aticles</Link></li>
+      </ul>
+    </nav>
+  );
+};
