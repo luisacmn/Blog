7e51fc64da9dfa34c36f4050241afe044cc94fdd
Luisa Novaes <luisanovaes.arq@gmail.com>
2024-04-27 15:31:16 -0300

    add: login page and create account routes and pages

---
+++ src/hooks/useUser.jsx
@@ -0,0 +1,5 @@
+export const useUser = () => {
+    return (
+      <h1>useUser</h1>
+    )
+}
---
+++ src/pages/CreateAccountPage.jsx
@@ -0,0 +1,5 @@
+export const CreateAccountPage = () => {
+    return (
+      <h1>Create Account</h1>
+    )
+  }
